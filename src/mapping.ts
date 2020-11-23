272614a98af9fe21c463755e8cbe9f7e2d0b0d90
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2020-11-23 20:36:06 +0200

    generate code

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,8 @@
-import { MetadataCreated, MetadataUpdated } from '../generated/Metadata/Metadata'
-import { Metadata } from '../generated/schema'
+import { MetadataCreated, MetadataUpdated } from './types/Metadata/Metadata'
+import { Metadata } from './types/schema'
 
 export function handleMetadataCreated(event: MetadataCreated): void {
-  let metadata = new Metadata(event.params.id.toHex())
+  let metadata = new Metadata(event.transaction.from.toHex())
   metadata.dataToken = event.params.dataToken
   metadata.createdBy = event.params.createdBy
   metadata.flags = event.params.flags
@@ -11,13 +11,13 @@ export function handleMetadataCreated(event: MetadataCreated): void {
 }
 
 export function handleMetadataUpdated(event: MetadataUpdated): void {
-  let id = event.params.id.toHex()
+  let id = event.transaction.from.toHex()
   let metadata = Metadata.load(id)
   if (metadata == null) {
     metadata = new Metadata(id)
   }
   metadata.dataToken = event.params.dataToken
-  metadata.createdBy = event.params.createdBy
+  metadata.createdBy = event.params.updatedBy
   metadata.flags = event.params.flags
   metadata.data = event.params.data
   metadata.save()
